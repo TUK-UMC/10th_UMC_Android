1073557b45e93bfd7d48584eab110db80401ebe1
Sungmin <lsm0205l56@gmail.com>
2026-03-28 16:06:45 +0900

    refactor: mainActivity 및 activityMain 리팩토링

--- damdam/UMC_Work_2nd/app/src/main/java/com/sungs/myapplication/MainActivity.kt
+++ damdam/UMC_Work_2nd/app/src/main/java/com/sungs/myapplication/MainActivity.kt
@@ -1,10 +1,8 @@
 package com.sungs.myapplication
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
 import com.sungs.myapplication.databinding.ActivityMainBinding
 import com.sungs.myapplication.fragment.BagFragment
 import com.sungs.myapplication.fragment.HomeFragment
@@ -14,58 +12,34 @@ import com.sungs.myapplication.fragment.WishlistFragment
 
 class MainActivity : AppCompatActivity() {
 
-    // 뷰바인딩 세팅
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // 바인딩 초기화
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // HomeFragment
         if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.fragment_container, HomeFragment())
-                .commit()
+            replaceFragment(HomeFragment())
         }
 
-        // 바텀 네비게이션 탭 클릭 이벤트 처리
         binding.bottomNav.setOnItemSelectedListener { item ->
             when (item.itemId) {
-                R.id.nav_home -> {
-                    supportFragmentManager.beginTransaction()
-                        .replace(R.id.fragment_container, HomeFragment())
-                        .commit()
-                    true
-                }
-                R.id.nav_shop -> {
-                    supportFragmentManager.beginTransaction()
-                        .replace(R.id.fragment_container, ShopFragment())
-                        .commit()
-                    true
-                }
-                R.id.nav_wishlist -> {
-                    supportFragmentManager.beginTransaction()
-                        .replace(R.id.fragment_container, WishlistFragment())
-                        .commit()
-                    true
-                }
-                R.id.nav_bag -> {
-                    supportFragmentManager.beginTransaction()
-                        .replace(R.id.fragment_container, BagFragment())
-                        .commit()
-                    true
-                }
-                R.id.nav_profile -> {
-                    supportFragmentManager.beginTransaction()
-                        .replace(R.id.fragment_container, ProfileFragment())
-                        .commit()
-                    true
-                }
-                else -> false
+                R.id.nav_home -> replaceFragment(HomeFragment())
+                R.id.nav_shop -> replaceFragment(ShopFragment())
+                R.id.nav_wishlist -> replaceFragment(WishlistFragment())
+                R.id.nav_bag -> replaceFragment(BagFragment())
+                R.id.nav_profile -> replaceFragment(ProfileFragment())
+                else -> return@setOnItemSelectedListener false
             }
+            true
         }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .commit()
     }
 }
