54a4fc698628ee88a4bcf8d63749e05f80854c07
Tyler <lyon1039@naver.com>
2026-03-31 02:43:18 +0900

    week2

---
+++ week2/app/src/main/java/com/example/week2/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.week2
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.example.week2.fragment.HomeFragment
+import com.example.week2.fragment.ShopFragment
+import com.example.week2.fragment.WishlistFragment
+import com.example.week2.fragment.BagFragment
+import com.example.week2.fragment.ProfileFragment
+import com.example.week2.databinding.ActivityMainBinding
+import com.example.week2.*
+
+private val fragment: Any
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        if (savedInstanceState == null) {
+            replaceFragment(HomeFragment())
+        }
+        //bottom
+        binding.bottomNav.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_home -> replaceFragment(HomeFragment())
+                R.id.nav_shop -> replaceFragment(ShopFragment())
+                R.id.nav_wishlist -> replaceFragment(WishlistFragment())
+                R.id.nav_bag -> replaceFragment(BagFragment())
+                R.id.nav_profile -> replaceFragment(ProfileFragment())
+                else -> return@setOnItemSelectedListener false
+            }
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .commit()
+    }
+}
