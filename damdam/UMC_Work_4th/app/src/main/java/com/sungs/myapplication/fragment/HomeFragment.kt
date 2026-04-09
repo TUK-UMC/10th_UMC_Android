15ba7182b4cd702f969780245b4404c1bda3026e
Sungmin <lsm0205l56@gmail.com>
2026-04-09 20:38:40 +0900

    feat: HomeFragment 수정

--- damdam/UMC_Work_4th/app/src/main/java/com/sungs/myapplication/fragment/HomeFragment.kt
+++ damdam/UMC_Work_4th/app/src/main/java/com/sungs/myapplication/fragment/HomeFragment.kt
@@ -5,23 +5,25 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.GridLayoutManager
 import com.sungs.myapplication.R
 import com.sungs.myapplication.adapter.ProductAdapter
 import com.sungs.myapplication.data.ProductData
+import com.sungs.myapplication.data.ProductDataStore
 import com.sungs.myapplication.databinding.FragmentHomeBinding
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
 
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
 
-    private val productList = listOf(
+    private val defaultProducts = listOf(
         ProductData("Air Jordan 1000R", "Shoes", "US$189", R.drawable.img_dunk_low),
         ProductData("Nike Air Force 1 '07", "Shoes", "US$115", R.drawable.img_air_force_1)
     )
-
-    private val productAdapter by lazy { ProductAdapter(productList) }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -34,8 +36,17 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.rvHomeProducts.adapter = productAdapter
         binding.rvHomeProducts.layoutManager = GridLayoutManager(requireContext(), 2)
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            val existing = ProductDataStore.getHomeProducts(requireContext()).first()
+            if (existing.isEmpty()) {
+                ProductDataStore.saveHomeProducts(requireContext(), defaultProducts)
+            }
+            ProductDataStore.getHomeProducts(requireContext()).collect { products ->
+                binding.rvHomeProducts.adapter = ProductAdapter(products)
+            }
+        }
     }
 
     override fun onDestroyView() {
