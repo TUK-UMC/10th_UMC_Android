f66a87c39a86bf33cde7ae08d46b77801aeba08a
Sungmin <lsm0205l56@gmail.com>
2026-04-18 20:30:57 +0900

    feat: 의존성 추가

--- damdam/UMC_Work_5th/app/build.gradle.kts
+++ damdam/UMC_Work_5th/app/build.gradle.kts
@@ -52,4 +52,10 @@ dependencies {
     androidTestImplementation(libs.androidx.espresso.core)
     implementation("androidx.datastore:datastore-preferences:1.2.1")
     implementation("com.google.code.gson:gson:2.13.2")
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
+    implementation("com.github.bumptech.glide:glide:4.16.0")
 }
