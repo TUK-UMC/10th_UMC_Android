57c9e6011cdb4b54fbd710dc992a2c6c9f67e315
Sungmin <lsm0205l56@gmail.com>
2026-05-15 19:53:33 +0900

    feat:  MainActivity 전환

--- damdam/UMC_Work_7th/app/src/main/java/com/sungs/myapplication/MainActivity.kt
+++ damdam/UMC_Work_7th/app/src/main/java/com/sungs/myapplication/MainActivity.kt
@@ -1,45 +1,20 @@
 package com.sungs.myapplication
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
-import com.sungs.myapplication.databinding.ActivityMainBinding
-import com.sungs.myapplication.fragment.BagFragment
-import com.sungs.myapplication.fragment.HomeFragment
-import com.sungs.myapplication.fragment.ProfileFragment
-import com.sungs.myapplication.fragment.ShopFragment
-import com.sungs.myapplication.fragment.WishlistFragment
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.sungs.myapplication.ui.MainScreen
+import com.sungs.myapplication.ui.theme.MyApplicationTheme
 
-class MainActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivityMainBinding
-
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        if (savedInstanceState == null) {
-            replaceFragment(HomeFragment())
-        }
-
-        binding.bottomNav.setOnItemSelectedListener { item ->
-            when (item.itemId) {
-                R.id.nav_home -> replaceFragment(HomeFragment())
-                R.id.nav_shop -> replaceFragment(ShopFragment())
-                R.id.nav_wishlist -> replaceFragment(WishlistFragment())
-                R.id.nav_bag -> replaceFragment(BagFragment())
-                R.id.nav_profile -> replaceFragment(ProfileFragment())
-                else -> return@setOnItemSelectedListener false
+        enableEdgeToEdge()
+        setContent {
+            MyApplicationTheme {
+                MainScreen()
             }
-            true
         }
-    }
-
-    private fun replaceFragment(fragment: Fragment) {
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.fragment_container, fragment)
-            .commit()
     }
 }
